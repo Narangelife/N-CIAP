0ed289daa1c902caedb5cf9d092ca8eba4fe4f5b
Narangelife <narange@scriptarts.jp>
2021-02-09 23:08:28 +0900

    First-commit

---
+++ CIAP/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿
+namespace CIAP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(14, 21);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(193, 19);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(14, 62);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(280, 30);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "SAVE";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(223, 21);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(70, 19);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Folder";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(307, 111);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "N-CIAP";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.Button button2;
+
+        private System.Windows.Forms.Button button1;
+
+        private System.Windows.Forms.TextBox textBox1;
+
+        #endregion
+    }
+}
+
---
+++ CIAP/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CIAP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            //throw new System.NotImplementedException();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Image img = Clipboard.GetImage();
+            Guid uuid = Guid.NewGuid();
+            if (img != null)
+            {
+                ImageFormat fmt = ImageFormat.Png;
+                try
+                {
+                    img.Save(textBox1.Text + @"/" + uuid.ToString() + ".png", fmt);
+                    MessageBox.Show("正しく保存されました", "Saved", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                }
+                catch (Exception exception)
+                {
+                    MessageBox.Show("正しいパスが入力されていないか、画像がクリップボードに存在しません", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    Console.WriteLine(exception);
+                    //throw;
+                }
+            }
+            //throw new System.NotImplementedException();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog
+            {
+                Description = "フォルダを選択してください",
+                RootFolder = Environment.SpecialFolder.Desktop,
+                SelectedPath = @"C:\Windows",
+                ShowNewFolderButton = true
+            };
+            if (fbd.ShowDialog((this)) == DialogResult.OK)
+            {
+                textBox1.Text = fbd.SelectedPath;
+            }
+            //throw new System.NotImplementedException();
+        }
+    }
+}
